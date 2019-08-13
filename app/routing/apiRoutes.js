3a9fa498ac344499a677cde4ab6f7e6cbd339d1b
Bob McDonough <bobmcdonough215@gmail.com>
2019-08-13 17:36:26 -0400

    changed jQuery and if, if else statements.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,59 +5,29 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        var difference;
-        var bestMatch = {
-            name: "",
-            photo: "",
-            friendDifference: 1000
-        };
-        var userData = req.body;
-        var userName = userData.name;
-        var userScores = userData.scores;
+        var maxDifference = Infinity;
+        var bestMatch = {};
+        console.log(req.body);
+        for (i = 0; i < friends.length; i++) {
+            var totalDifference = 0;
+ 
+            for (j = 0; j < friends[i].scores.length; j++) { 
+            var friendsScore = friends[i].scores[j];
+            var userScore = req.body.scores[j];
+            console.log(friendsScore, userScore);
+            totalDifference+=Math.abs(parseInt(friendsScore)-parseInt(userScore));
+            console.log(totalDifference);
 
-        // var b = userScores.map(function (item) {
-        //     return parseInt(item, 10);
-        // });
-        // userData = {
-        //     name: req.body.name,
-        //     photo: req.body.photo,
-        //     scores: b
-        // };
-        console.log("Name: " + userName);
-        console.log("User Score: " + userScores);
-
-        // var sum = b.reduce((a, b) => a + b, 0);
-        // console.log("Sum of all users score " + sum);
-        console.log("Best match " + bestMatch.friendDifference);
-        console.log("++++++++++++++++++++++=====================");
-
-        for (var i = 0; i < friends.length; i++) {
-            console.log(friends[i].name);
-            var newFriend = friends[i];
-            difference = 0;
-            console.log("Total Diff " + difference);
-            console.log("Best match friend diff " + bestMatch.friendDifference);
-
-            for (var j = 0; j < newFriend.scores.length; j++) {
-                console.log(newFriend.scores[j]);
-                console.log(userScores[j]);
-               
-                difference += Math.abs(userScores[j] - newFriend.scores[j]);
-                console.log("-------------------------> " + difference);
-
-                if (difference <= bestMatch.friendDifference) {
-                    bestMatch.name = friends[i].name;
-                    bestMatch.photo = friends[i].photo;
-                    bestMatch.friendDifference = difference;
-                }
+            };
+            if(totalDifference<maxDifference){
+                maxDifference=totalDifference
+                bestMatch= friends[i]
             }
 
-            // console.log(totalDifference + "Total Difference");
-        }
-        // console.log(bestMatch);
-        friends.push(userData);
-        console.log("New User Added");
-        console.log(userData);
+        };
+console.log(bestMatch);
+console.log(maxDifference);
+        friends.push(req.body);
         res.json(bestMatch);
     });
 };
